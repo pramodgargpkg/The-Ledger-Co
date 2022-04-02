385619686c803b9dd6074c9b88140a005a972849
pramodgarg <pramod.garg.pkg@gmail.com>
2022-04-03 01:30:05 +0530

    Class name changed to GeekTrust

--- src/Geektrust.java
+++ src/Geektrust.java
@@ -3,7 +3,7 @@ import Processors.IFileProcessor;
 import Resources.ErrorMessages;
 import Resources.Utils;
 
-public class Main {
+public class Geektrust {
     public static void main(String[] args) throws Exception {
         if (args.length == 0) {
             Utils.ConsoleLogError(ErrorMessages.CommandNotFound());
